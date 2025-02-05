a8a350047a732b89eb6558c6982b9724e58c9f55
Mahmoud ELshaarawy <MahmoudFarag1842001@gmail.com>
2025-02-05 14:57:09 +0200

    finished UI of the OnBoardingScreen

---
+++ app/src/main/java/com/example/myapplication/ui/presentation/log_in/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.presentation.log_in
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+
+    }
+}
